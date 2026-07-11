663443a9eeccf5d4f0a72c0668010bf76aaa836b
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2026-07-11 12:10:12 -0400

    Cleanup

--- update_contributor_data.py
+++ update_contributor_data.py
@@ -1,14 +1,3 @@
-# This script updates the below values for the PyTorch GitHub Repos Dataset (https://huggingface.co/datasets/TylerHilbert/PyTorchConference2025_GithubRepos)
-#   contributors_all
-#   contributors_2026_q1
-#   contributors_2025
-#   contributors_2024
-#   contributors_2023
-# This script clones and overwrites PyTorchConference2025_GithubRepos.json
-# Sorting by most contributors is toggled with `save_updated_dataset`.
-# This script does not push the update to HuggingFace.
-# The values are updated by cloning each `repo_link` and using a git log command.
-
 import os
 import json
 import time
@@ -135,68 +124,4 @@ def print_repo_stats(repos_list):
 def get_repo_dir(repo_link):
     return repo_link.split('https://github.com/')[1].replace('/', '__')
 
-'''
-# Updates the following fields for `repos`:
-#   contributors_all
-#   contributors_2026_q1
-#   contributors_2025
-#   contributors_2024
-#   contributors_2023
-# TODO may have a bug for if '|' is in name
-def update_contributors(repos):
-    print ('Updating contributors')
-    root_dir = os.getcwd()
-    for repo in repos:
-        print (f'Updating {repo["repo_name"]}')
-        contributors_all = set()
-        contributors_2026_q1 = set()
-        contributors_2025 = set()
-        contributors_2024 = set()
-        contributors_2023 = set()
-
-        os.chdir(get_repo_dir(repo))
-        result = subprocess.run(
-            ["git", "log", "--pretty=format:%ad|%an", "--date=format:%Y-%m-%d"], # Uses name as unique field
-            #["git", "log", "--pretty=format:%ad|%ae", "--date=format:%Y-%m-%d"], # Uses email as unique field
-            capture_output=True,
-            encoding='utf-8',
-            errors='replace'
-        )
-        for line in result.stdout.strip().split('\n'):
-            date_str, name = line.split('|', 1) # TODO this may cause a crash if there are no commits
-            name = name.lower()
-            contributors_all.add(name)
-            if date_str.startswith(('2026-01', '2026-02', '2026-03')): # First quarter of 2026
-                contributors_2026_q1.add(name)
-            elif '2025' in date_str:
-                contributors_2025.add(name)
-            elif '2024' in date_str:
-                contributors_2024.add(name)
-            elif '2023' in date_str:
-                contributors_2023.add(name)
-        
-        repo['contributors_all'] = len(contributors_all)
-        repo['contributors_2026_q1'] = len(contributors_2026_q1)
-        repo['contributors_2025'] = len(contributors_2025)
-        repo['contributors_2024'] = len(contributors_2024)
-        repo['contributors_2023'] = len(contributors_2023)
-        os.chdir(root_dir)
-
-    print()
-    return repos
-
-# Saves the dataset with contributors updated back to the .json file
-def save_updated_dataset(repos, sort_by_contributors):
-    dataset_path = 'PyTorchConference2025_GithubRepos/PyTorchConference2025_GithubRepos.json'
-    print(f'Saving updated dataset to {dataset_path}')
-
-    if sort_by_contributors:
-        repos.sort(key=lambda r: r['contributors_all'], reverse=True)
-
-    with open(dataset_path, 'w', encoding='utf-8') as file:
-        json.dump(repos, file, indent=2, ensure_ascii=False)
-    
-    print(f'Successfully saved {len(repos)} entries to {dataset_path}')
-    print()
-'''
 main()
