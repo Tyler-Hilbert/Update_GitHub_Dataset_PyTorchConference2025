0fe127c30b39c39bef223352c35a0bde62387627
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2026-07-10 20:04:09 -0400

    Partial Refactor. Repo list is a list of urls which should make it easier to use other sources. Now it only prints instead of updates the original list though.

--- update_contributor_data.py
+++ update_contributor_data.py
@@ -15,27 +15,32 @@ import time
 import subprocess
 
 def main():
-    clone_dataset()
-    pull_dataset()
-    repos = clone_and_pull_repos()
-    repos_updated = update_contributors(repos)
-    save_updated_dataset(repos_updated, True)
+    repos_list = get_repos_list()
+    clone_and_pull_repos(repos_list)
+    print_repo_stats(repos_list)
+
+# Returns list of repo urls
+def get_repos_list():
+    clone_repos_list('https://huggingface.co/datasets/TylerHilbert/PyTorchConference2025_GithubRepos')
+    pull_repos_list('PyTorchConference2025_GithubRepos')
+    repos_list = extract_repos_list()
+    return repos_list
 
 # Clones the dataset of GitHub repos (not each repo)
 # TODO verify git is installed on machine before running
-def clone_dataset():
-    print ('Cloning original dataset')
-    clone_dataset_command = 'git clone https://huggingface.co/datasets/TylerHilbert/PyTorchConference2025_GithubRepos'
+def clone_repos_list(huggingface_url):
+    print ('Cloning Repos list')
+    clone_dataset_command = f'git clone {huggingface_url}'
     print (f'${clone_dataset_command}')
     os.system(clone_dataset_command)
     print()
 
-# Does a git pull to verify dataset is updated
-def pull_dataset():
-    print ('Updating original dataset')
+# Executes Git Pull on dataset is updated
+def pull_repos_list(local_repo_list_name):
+    print ("Pulling repos list (making sure it's updated)")
 
     root_dir = os.getcwd()
-    os.chdir('PyTorchConference2025_GithubRepos')
+    os.chdir(f'{local_repo_list_name}')
     update_dataset_command = 'git pull'
     print (f'${update_dataset_command}')
     os.system(update_dataset_command)
@@ -43,22 +48,30 @@ def pull_dataset():
 
     print()
 
-# Runs `git clone` and `git pull` for each `repo_link` in dataset .json file
-def clone_and_pull_repos():
-    print ('Cloning repos')
+# Returns the list of repo urls
+# TODO don't have this hardcoded
+def extract_repos_list():
     dataset_path = 'PyTorchConference2025_GithubRepos/PyTorchConference2025_GithubRepos.json'
     with open(dataset_path, 'r', encoding='utf-8') as file:
         repos = json.load(file)
-    
+    repos_list = []
     for repo in repos:
+        repos_list.append(repo['repo_link'])
+    return repos_list
+
+# Runs `git clone` and `git pull` for each of the urls in `repos_list`
+def clone_and_pull_repos(repos_list):
+    print ('Cloning repos')
+    
+    for repo in repos_list:
         # Verify repo_link is a github url
-        if 'github.com' not in repo['repo_link'].lower():
-            print (f'error: repo["repo_link"] invalid github link')
+        if 'github.com' not in repo.lower():
+            print (f'error: repo invalid github link')
             exit()
         
         # Clone repo
         custom_dir_name = get_repo_dir(repo)
-        clone_command = f'git clone {repo["repo_link"]} {custom_dir_name}'
+        clone_command = f'git clone {repo} {custom_dir_name}'
         print (clone_command)
         os.system(clone_command)
         time.sleep(2)
@@ -74,13 +87,55 @@ def clone_and_pull_repos():
         print ()
     
     print()
-    return repos
 
-# Returns unique name based on the repo_url
+# TODO may have a bug for if '|' is in name
+def print_repo_stats(repos_list):
+    print ('Iterating over repos list')
+    root_dir = os.getcwd()
+    for repo in repos_list:
+        print (f'Processing {repo}')
+        contributors_all = set()
+        contributors_2026_q1 = set()
+        contributors_2025 = set()
+        contributors_2024 = set()
+        contributors_2023 = set()
+
+        os.chdir(get_repo_dir(repo))
+        result = subprocess.run(
+            ["git", "log", "--pretty=format:%ad|%an", "--date=format:%Y-%m-%d"], # Uses name as unique field
+            #["git", "log", "--pretty=format:%ad|%ae", "--date=format:%Y-%m-%d"], # Uses email as unique field
+            capture_output=True,
+            encoding='utf-8',
+            errors='replace'
+        )
+        for line in result.stdout.strip().split('\n'):
+            date_str, name = line.split('|', 1) # TODO this may cause a crash if there are no commits
+            name = name.lower()
+            contributors_all.add(name)
+            if date_str.startswith(('2026-01', '2026-02', '2026-03')): # First quarter of 2026
+                contributors_2026_q1.add(name)
+            elif '2025' in date_str:
+                contributors_2025.add(name)
+            elif '2024' in date_str:
+                contributors_2024.add(name)
+            elif '2023' in date_str:
+                contributors_2023.add(name)
+        
+        print ('contributors_all', len(contributors_all))
+        print ('contributors_2026_q1', len(contributors_2026_q1))
+        print ('contributors_2025', len(contributors_2025))
+        print ('contributors_2024', len(contributors_2024))
+        print ('contributors_2023', len(contributors_2023))
+        os.chdir(root_dir)
+
+    print()
+
+# Returns unique name based on the url
 # TODO fix bug where repo with '__' in name could cause duplicate values
-def get_repo_dir(repo):
-    return repo['repo_link'].split('https://github.com/')[1].replace('/', '__')
+def get_repo_dir(repo_link):
+    return repo_link.split('https://github.com/')[1].replace('/', '__')
 
+'''
 # Updates the following fields for `repos`:
 #   contributors_all
 #   contributors_2026_q1
@@ -143,5 +198,5 @@ def save_updated_dataset(repos, sort_by_contributors):
     
     print(f'Successfully saved {len(repos)} entries to {dataset_path}')
     print()
-
+'''
 main()
